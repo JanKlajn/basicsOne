c4b5fd0b63c68233573810898e90c72e61dc340e
JanKlajn <wawer527@gmail.com>
2021-12-07 02:43:23 +0100

    start

--- basicsOne/urls.py
+++ basicsOne/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import archive
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', archive),
+    path('', archive)
 ]
